48caae856c635c8a496b4c1a00b257ce3e64385e
Randy Tarampi <randy.tarampi@smunch.co>
2019-07-29 15:25:19 +0200

    Export `withScrollingConsumer` for outside consumption.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import { withScrollingConsumer } from './scrollContext';
 import ScrollingProvider from './ScrollingProvider';
 import Section from './Section';
 import SectionLink from './SectionLink';
 import SectionLinks from './SectionLinks';
 
-export { ScrollingProvider, Section, SectionLink, SectionLinks };
+export { ScrollingProvider, Section, SectionLink, SectionLinks, withScrollingConsumer };
